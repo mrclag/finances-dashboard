b900a7e8b28f32fc31e3a730e05818e4daceed29
Matt Clagett <mattrclagett@gmail.com>
2023-04-02 22:50:23 -0600

    Redux toolkit query api + types for products

--- server/index.js
+++ server/index.js
@@ -39,6 +39,6 @@ mongoose
     // seeded db
     // await mongoose.connection.db.dropDatabase()
     // KPI.insertMany(kpis)
-    // Product.insertMany(products)
+  // Product.insertMany(products)
   })
   .catch((error) => console.log(`${error} did not connect`))

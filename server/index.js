f8e9cbbabb0010aab9b9ea7e123c11be67a0ef60
mrclag <50685701+mrclag@users.noreply.github.com>
2023-04-03 16:22:19 -0600

    Add transactions route and seed db (#3)

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from "helmet"
 import morgan from "morgan"
 import kpiRoutes from "./routes/kpi.js"
 import KPI from "./models/KPI.js"
-import { kpis, products } from "./data/data.js"
+import { kpis, products, transactions } from "./data/data.js"
 import Product from "./models/Product.js"
 import productRoutes from "./routes/product.js"
+import transactionRoutes from "./routes/transaction.js"
+import Transaction from "./models/Transaction.js"
 
 // CONFIG
 dotenv.config()
@@ -23,6 +25,7 @@ app.use(cors())
 
 app.use("/kpi", kpiRoutes)
 app.use("/product", productRoutes)
+app.use("/transaction", transactionRoutes)
 
 const PORT = process.env.PORT || 9000
 
@@ -39,6 +42,7 @@ mongoose
     // seeded db
     // await mongoose.connection.db.dropDatabase()
     // KPI.insertMany(kpis)
-  // Product.insertMany(products)
+    // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
   })
   .catch((error) => console.log(`${error} did not connect`))

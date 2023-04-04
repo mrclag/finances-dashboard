0b23e289354f4c7e85df805c90b2ccd4bffbfb39
mrclag <50685701+mrclag@users.noreply.github.com>
2023-04-04 12:43:18 -0600

    Product and transactions data lists, plus revenue predictions page (#6)
    * Add transactions route and seed db
    
    * Setup redux toolkit query and types for transactions
    
    * Add MUI grid for product list
    
    * Styling scrollbar
    
    * Finish transcations tables and summary, fix for transaction route
    
    * remove seed data
    
    * Add revenue predictions page with regression line
    
    * Typo fix + hiding data import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { themeSettings } from "./theme"
 import Navbar from "@/scenes/navbar"
 import Dashboard from "@/scenes/dashboard"
+import Predictions from "./scenes/predictions"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -18,7 +19,7 @@ function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/predictions" element={<div>predictions</div>} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>

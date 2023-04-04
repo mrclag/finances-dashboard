67a47874bde729129a06ebb5462aeb1fb1db8ec7
mrclag <50685701+mrclag@users.noreply.github.com>
2023-04-03 21:07:37 -0600

    Add transactions data and summaries (#5)
    * Add transactions route and seed db
    
    * Setup redux toolkit query and types for transactions
    
    * Add MUI grid for product list
    
    * Styling scrollbar
    
    * Finish transcations tables and summary, fix for transaction route

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,11 +1,13 @@
 import express from "express"
-import Product from "../models/Product.js"
+import Transaction from "../models/Transaction.js"\
 
 const router = express.Router()
 
 router.get("/transactions", async (req, res) => {
   try {
-    const transactions = await Product.find().limit(50).sort({ createdOn: -1 })
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdOn: -1 })
     res.status(200).json(transactions)
   } catch (error) {
     res.status(404).json({ message: error.message })
